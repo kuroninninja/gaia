b062bbbd59bba88d0a01e0c7779ce17945fe1e85
Henry ᛯ <henry.h.george2012@gmail.com>
2025-09-12 20:54:18 -0700

    Create movecomm.cs

---
+++ MoveComm/movecomm.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Commands.Move
+{
+    public static class MoveCommand
+    {
+        public static void FindMoveCase(string userCommand)
+        {
+            // Depending on the player location, call a different function
+            // which itself uses the userCommand argument to determine
+            // destination and behavior.
+        }
+    }
+}
