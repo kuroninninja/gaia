414ecae2b1f2298d703ddefe766f5f0d114289b1
Henry ᛯ <henry.h.george2012@gmail.com>
2025-09-12 20:52:15 -0700

    Create global_info.cs

---
+++ global_info.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace gaia.info
+{
+    public static class Player
+    {
+        public static List<string> inventory = [];
+    }
+
+    public static class GeneralInfo
+    {
+        public static ConsoleColor originalColor = Console.ForegroundColor;
+    }
+}
