5b14f66e0aa0773ebc27ff27874ca077299deef9
Henry ᛯ <henry.h.george2012@gmail.com>
2025-09-13 17:55:55 -0700

    Update and rename global_info.cs to GlobalInfo.cs

--- GlobalInfo.cs
+++ GlobalInfo.cs
@@ -5,10 +5,13 @@ namespace gaia.info
     public static class Player
     {
         public static List<string> inventory = [];
+        public static Location location = Location.StartingBed;
+        public static Location previousLocation = Location.Placeholder;
     }
 
     public static class GeneralInfo
     {
         public static ConsoleColor originalColor = Console.ForegroundColor;
+        public static bool showDescription = true;
     }
 }
