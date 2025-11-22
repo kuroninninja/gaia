4fd7516405e029067e4e1e96f7d5af1a531a1c13
Henry <henry.h.george2012@gmail.com>
2025-11-21 19:15:37 -0800

    Implement Prompter class for user input handling

---
+++ src/Prompter.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace gaia.prompter
+{
+    public class Prompter
+    {
+        public static void Prompt()
+        {
+            // Set previous location
+            Player.previousLocation = Player.location;
+            // Ask for input
+            Console.Write(">  ");
+            string input = Console.ReadLine().ToLower(); // Lowercase for simplicity
+            // Variations of commands
+            string[] exitStatements = ["quit"];
+            string[] moveStatements = ["move", "go", "leave", "get up", "get out"];
+            // Check if any of the commands align with the input
+            bool startsWithMove = moveStatements.Any(comm => input.StartsWith(comm));
+            // For each possible command, check if it aligns: if it does, run said command
+            if (exitStatements.Contains(input))
+            {
+                Environment.Exit(0);
+            }
+            else if (startsWithMove)
+            {
+                MoveCommand.FindMoveCase(input);
+            }
+            
+            // Check if player has not moved:
+            if (Player.previousLocation == Player.location)
+            {
+                // if so, don't show description;
+                GeneralInfo.showDescription = false;
+            }
+            else
+            {
+                // if not, do show descriptpion
+                GeneralInfo.showDescription = true;
+            }
+        }
+    }
+}
