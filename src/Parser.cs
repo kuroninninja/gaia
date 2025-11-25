fc3252c9e9462a4871e9f21d7b4c9681a68dab27
Henry <henry.h.george2012@gmail.com>
2025-11-24 17:30:10 -0800

    Implement Parse method to clean user input

--- src/Parser.cs
+++ src/Parser.cs
@@ -1,3 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 namespace gaia.parser
+{
+    public class Parser
+    {
+        public static List<string> Parse(string userInput)
+        {
+            // Convert input to array of characters
+            char[] inputArray = userInput.ToCharArray();
+            List<char> inputLetters = userInput.ToList();
+            // Every punctuation mark
+            char[] punctuationMarks = [',', '.', ';', ':', '!', '?'];
+            // Loop through each and remove punctuation
+            for(int i = inputLetters.Count - 1; i >= 0; i--)
+            {
+                if (punctuationMarks.Contains(inputLetters[i]))
+                {
+                    inputLetters.RemoveAt(i);
+                }
+            }
+            // Join list back to string
+            string afterPunct = string.Join("", inputLetters);
+            string[] aPArray = afterPunct.Split(' ');
+            List<string> textList = aPArray.ToList();
+            return textList;
+        }
+    }
+}
