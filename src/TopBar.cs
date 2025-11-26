0658396e16702f0b0233b3fe79f06719e75b3748
Henry <henry.h.george2012@gmail.com>
2025-11-26 15:55:14 -0800

    Fix TopBar.cs cursor positioning and width calculation

--- src/TopBar.cs
+++ src/TopBar.cs
@@ -9,18 +9,21 @@ namespace gaia.topbar
             int currentCursorTop = Console.CursorTop;
             int currentCursorLeft = Console.CursorLeft;
             int topLine = 0;
+            int width = Console.WindowWidth;
 
             Console.SetCursorPosition(0, topLine);
 
             string barText = "GAIA";
             string barEndText = $"{Player.location.GetDescription()}";
+            string barMiddleText = new string(' ', width - (barText.Length + barEndText.Length));
+            
+            barText = barText + barMiddleText + barEndText;
             
 
             Console.ForegroundColor = ConsoleColor.Black;
             Console.BackgroundColor = ConsoleColor.White;
 
             // Clear line
-            int width = Console.WindowWidth;
             Console.Write(new string(' ', width - 1));
             Console.SetCursorPosition(0, topLine);
 
