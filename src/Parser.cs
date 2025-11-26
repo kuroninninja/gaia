24b818a408cbe95b670a056e53f7c72ca692c39b
Henry <henry.h.george2012@gmail.com>
2025-11-26 11:07:35 -0800

    Refactor Parse method to handle tokens and remove fillers

--- src/Parser.cs
+++ src/Parser.cs
@@ -1,17 +1,20 @@
 using System;
 using System.Collections.Generic;
+using gaia.tokenizer;
 
 namespace gaia.parser
 {
     public class Parser
     {
-        public static List<string> Parse(string userInput)
+        public static List<Token> Parse(string userInput)
         {
             // Convert input to array of characters
             char[] inputArray = userInput.ToCharArray();
             List<char> inputLetters = userInput.ToList();
+
             // Every punctuation mark
             char[] punctuationMarks = [',', '.', ';', ':', '!', '?'];
+
             // Loop through each and remove punctuation
             for(int i = inputLetters.Count - 1; i >= 0; i--)
             {
@@ -20,11 +23,25 @@ namespace gaia.parser
                     inputLetters.RemoveAt(i);
                 }
             }
+
             // Join list back to string
             string afterPunct = string.Join("", inputLetters);
-            string[] aPArray = afterPunct.Split(' ');
-            List<string> textList = aPArray.ToList();
-            return textList;
+            string[] noPunct = afterPunct.Split(' ');
+            List<string> textList = noPunct.ToList();
+
+            // Filler (unnecessary) words
+            string[] fillerWords = ["the", "a", "an"];
+
+            // Loop through each word and remove fillers
+            for(int i = textList.Count - 1; i >= 0; i--)
+            {
+                if (fillerWords.Contains(textList[i]))
+                {
+                    textList.RemoveAt(i);
+                } 
+            }
+
+            return Tokenizer.Tokenize(textList);
         }
     }
 }
