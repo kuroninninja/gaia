5801b21837f60b14ea7f4e4c6d4301e70c0d551f
Henry <henry.h.george2012@gmail.com>
2025-11-26 15:52:16 -0800

    Implement EnumExtensions for enum description retrieval
    Added a method to retrieve the description attribute of enum values.

--- src/Locations.cs
+++ src/Locations.cs
@@ -1,4 +1,6 @@
 using System;
+using System.ComponentModel;
+using System.Reflection;
 
 namespace gaia.locations 
 {
@@ -6,6 +8,21 @@ namespace gaia.locations
     public enum Location
     {
         Placeholder,
+        [Description("White Bed")]
         StartingBed
+    }
+
+    public static class EnumExtensions
+    {
+        public static string GetDescription(this Enum value)
+        {
+            FieldInfo field = value.GetType().GetField(value.ToString());
+
+            DescriptionAttribute attribute =
+                Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute))
+                as DescriptionAttribute;
+
+            return attribute == null ? value.ToString() : attribute.Description;
+        }
     }
 }
